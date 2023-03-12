4202c8b8d80dc28f99224ffa2d8aed4caa21c705
YanXH <YanXH@study.com>
2023-03-12 10:18:13 +0800

    master commit2

--- src/main/java/com/yanxh/git/GitTest.java
+++ src/main/java/com/yanxh/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hit4!");
         System.out.println("hello hit5!");
         System.out.println("hello hit6!");
+        System.out.println("master test!");
     }
 }
