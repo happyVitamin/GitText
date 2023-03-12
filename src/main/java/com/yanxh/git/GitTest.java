0399ac3199d494fba92095ca908a1766c490c688
YanXH <YanXH@study.com>
2023-03-12 10:31:22 +0800

    push commit

--- src/main/java/com/yanxh/git/GitTest.java
+++ src/main/java/com/yanxh/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello hit6!");
         System.out.println("master test!");
         System.out.println("hot_fix test!");
+        System.out.println("push test!");
     }
 }
