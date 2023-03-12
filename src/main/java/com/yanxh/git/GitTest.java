6b99bd4c7fe9ffac6ee0f47bfdf4aba8fc293c47
YanXH <YanXH@study.com>
2023-03-12 09:58:52 +0800

    second commit

--- src/main/java/com/yanxh/git/GitTest.java
+++ src/main/java/com/yanxh/git/GitTest.java
@@ -3,5 +3,8 @@ package com.yanxh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello hit!");
+        System.out.println("hello hit2!");
+        System.out.println("hello hit3!");
+        System.out.println("hello hit4!");
     }
 }
